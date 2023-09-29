4e055e2e471148cf52927a32d74866347ae30442
sristisravan <sristisravan@gmail.com>
2023-09-29 13:08:39 +0000

    Update tools.sh

--- assign-02/tools.sh
+++ assign-02/tools.sh
@@ -8,9 +8,3 @@ sudo service apache2 start
 /etc/init.d/vsftpd start 
 
 
-sudo ufw allow 80 # Allow DNS
-sudo ufw allow 21  # Allow FTP
-
-# Enable the firewall and allow SSH connections
-sudo ufw --force enable
-
